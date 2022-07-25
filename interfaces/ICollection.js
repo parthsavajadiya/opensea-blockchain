ae3ae6aa9580bdcb1ab384262bcef68b08b4bc04
parthsavajadiya <parthsavajadiya7443@gmail.com>
2022-07-25 17:39:00 +0530

    nft added

---
+++ interfaces/ICollection.js
@@ -0,0 +1,27 @@
+// export interface ICollection {
+//   allOwners: AllOwner[];
+//   bannerImageUrl: string;
+//   contractAddress: string;
+//   createdBy: CreatedBy;
+//   creator: string;
+//   description: string;
+//   floorPrice: number;
+//   imageUrl: string;
+//   title: string;
+//   volumeTraded: number;
+// }
+
+// interface AllOwner {
+//   _createdAt: Date;
+//   _id: string;
+//   _rev: string;
+//   _type: string;
+//   _updatedAt: Date;
+//   userName: string;
+//   walletAddress: string;
+// }
+
+// interface CreatedBy {
+//   _ref: string;
+//   _type: string;
+// }
